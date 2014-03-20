15add8ee7aaabaf8298ca6b1054fa3256f269b20
1090 <raspi@cinnamon>
2014-03-20 21:34:29 +0000

    three-phase concept

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ import serial
 import time
 import RPi.GPIO as GPIO
 
-SLEEP_TICK = 1
 MODEM_PORT = '/dev/ttyUSB0'
 DESTINATION_NUMBER = '+7xxxxxxxxxx'
 
 EOM_MARKERS = ['OK', 'ERROR']
+
+NORMAL_MODE_INTERVAL = 3
+SUSPICIOUS_MODE_INTERVAL = 1
+ALERT_MODE_INTERVAL = 3
+
+NORMAL_MODE_TOSUSPICIOUS_THRESHOLD = 2
+SUSPICIOUS_MODE_TOALERT_THRESHOLD = 4
+SUSPICIOUS_MODE_TONORMAL_THRESHOLD = 3
+ALERT_MODE_TONORMAL_THRESHOLD = 3
+
+STATE_NORMAL = 1
+STATE_SUSPICIOUS = 2
+STATE_ALERT = 3
 
 def str_send (port, data):
     print "<<" + data
@@ -46,19 +58,44 @@ def main():
 
     stty.close()
 
-    print "Watching"
-    alert_state = False
+    state = STATE_NORMAL
+    sleep_interval = NORMAL_MODE_INTERVAL
+    state_counter = 0
+    state_counter_neg = 0
+    print "Entered mode STATE_NORMAL"
     while True:
         status = GPIO.input(7)
-        if status and not alert_state:
-            ts = datetime.datetime.now()
-            print "[ALERT] @ %s"%ts
-            alert_state = True
-        if alert_state and not status:
-            alert_duration = datetime.datetime.now() - ts
-            print "[NORMAL] alert was %s"%alert_duration
-            alert_state = False
-        time.sleep(SLEEP_TICK)
+        state_counter = state_counter + int(status)
+        state_counter_neg = state_counter_neg + 1 - int(status)
+        if state == STATE_NORMAL and state_counter >= NORMAL_MODE_TOSUSPICIOUS_THRESHOLD:
+            ts_suspicious = datetime.datetime.now()
+            state = STATE_SUSPICIOUS
+            sleep_interval = SUSPICIOUS_MODE_INTERVAL
+            state_counter = 0
+            state_counter_neg = 0
+            print "[SUSPICIOUS] @ %s"%ts_suspicious
+        if state == STATE_SUSPICIOUS and state_counter >= SUSPICIOUS_MODE_TOALERT_THRESHOLD:
+            ts_alert = datetime.datetime.now()
+            state = STATE_ALERT
+            sleep_interval = ALERT_MODE_INTERVAL
+            state_counter = 0
+            state_counter_neg = 0
+            print "[ALERT] @ %s"%ts_alert
+        if state == STATE_SUSPICIOUS and state_counter_neg >= SUSPICIOUS_MODE_TONORMAL_THRESHOLD:
+            suspicious_duration = datetime.datetime.now() - ts_suspicious
+            print "[NORMAL] reverted back from SUSPICIOUS after %s s"%suspicious_duration
+            state = STATE_NORMAL
+            sleep_interval = NORMAL_MODE_INTERVAL
+            state_counter = 0
+            state_counter_neg = 0
+        if state == STATE_ALERT and state_counter_neg >= ALERT_MODE_TONORMAL_THRESHOLD:
+            alert_duration = datetime.datetime.now() - ts_alert
+            print "[NORMAL] reverted back from ALTER after %s s"%alert_duration
+            state = STATE_NORMAL
+            sleep_interval = NORMAL_MODE_INTERVAL
+            state_counter = 0
+            state_counter_neg = 0
+        time.sleep(sleep_interval)
 
 if __name__ == '__main__':
     main()
